a13a3673411eb8ab05efbfdcaa9fe62528af8b72
Dongil Cho <chodongil@yahoo.co.kr>
2025-11-14 11:45:32 +0900

    init

---
+++ spark-exam/src/main/java/example/spark/SimpleApp.java
@@ -0,0 +1,27 @@
+package example.spark;
+
+import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.SparkSession;
+
+
+public class SimpleApp {
+
+	public static void main(String[] args) {
+		String logFile = "C:/Temp/lockinglog1747383869427.log";
+		SparkSession spark = SparkSession.builder()
+				.appName("Simple Application")
+				.config("spark.master", "local")
+				.getOrCreate();
+
+		Dataset<String> logData = spark.read().textFile(logFile).cache();
+		
+		long numAs = logData.filter((FilterFunction<String>)s -> s.contains("a")).count();
+		long numBs = logData.filter((FilterFunction<String>)s -> s.contains("b")).count();
+		
+		System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+		
+		spark.stop();
+	}
+
+}
