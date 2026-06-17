7eb59caf75ff1351db4f5549ef08fad444dc8d02
Dongil Cho <chodongil@yahoo.co.kr>
2026-06-17 18:20:04 +0900

    미사용 코드 제거

--- hd-dbscan/src/main/java/org/hddbscan/dbscan/DBSCANMetadata.java
+++ hd-dbscan/src/main/java/org/hddbscan/dbscan/DBSCANMetadata.java
@@ -1,7 +1,7 @@
 package org.hddbscan.dbscan;
 
 import java.util.ArrayList;
-import java.util.Collections;
+//import java.util.Collections;
 import java.util.List;
 
 import org.hddbscan.dbscan.feature.DimensionConstraint;
@@ -14,18 +14,18 @@ public class DBSCANMetadata {
 	public DBSCANMetadata() {
 	}
 	
-	public void setEpsList(DimensionConstraint...constraintList) {
-		this.constraintList.clear();
-		Collections.addAll(this.constraintList, constraintList);
-	}
+//	public void setEpsList(DimensionConstraint...constraintList) {
+//		this.constraintList.clear();
+//		Collections.addAll(this.constraintList, constraintList);
+//	}
 	
 	public void addConstraint(DimensionConstraint eps) {
 		this.constraintList.add(eps);
 	}
 	
-	public void setConstraint(int index, DimensionConstraint eps) {
-		this.constraintList.set(index, eps);
-	}
+//	public void setConstraint(int index, DimensionConstraint eps) {
+//		this.constraintList.set(index, eps);
+//	}
 	
 	public int getConstraintCount() {
 		return this.constraintList.size();
