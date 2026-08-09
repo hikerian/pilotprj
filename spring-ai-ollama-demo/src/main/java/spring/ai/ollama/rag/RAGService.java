5dee5103e760473bbd4ea5fd211181cee1f10384
Dongil-Cho <chodongil@yahoo.co.kr>
2026-08-09 17:11:57 +0900

    ETL + RAG 진행중... Ollama + MariaDB Vector로 변환 예정

---
+++ spring-ai-ollama-demo/src/main/java/spring/ai/ollama/rag/RAGService.java
@@ -0,0 +1,18 @@
+package spring.ai.ollama.rag;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class RAGService {
+	private final Logger log = LoggerFactory.getLogger(RAGService.class);
+	
+	
+	public RAGService() {
+	}
+	
+	
+
+}
